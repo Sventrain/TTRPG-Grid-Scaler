0c8a0635c51ccf22c102be41fb72b4eec39e5a9f
hlandry3 <hlandry@uccs.edu>
2025-04-04 12:31:45 -0600

    4/4/25 commit

--- ttrpg-grid/src/App.jsx
+++ ttrpg-grid/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
+import Layout from './components/layout';
 import ImageUploader from "./components/ImageUploader";
 import './style.css';
 
 function App() {
-  const [imageURL, setImageURL] = useState(null)
+  {/*const [imageURL, setImageURL] = useState(null)*/}
 
   return (
-    <>
-      <h1 style={{textAlign: "center" }}>TTRPG Grid Scaler</h1>
-      <ImageUploader onImageSelected={setImageURL} />
-    </>
+    <Layout>
+      < ImageUploader />
+      {/*<h1 style={{textAlign: "center" }}>TTRPG Grid Scaler</h1>*/}
+      {/*<ImageUploader onImageSelected={setImageURL} />*/}
+    </Layout>
   );
 }
 
