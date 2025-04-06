22cf782d376bfb60efc25466d39f4d888ea97ee5
Eggie574 <eggie574@gmail.com>
2025-04-06 13:04:41 -0600

    Deleted html, didn't use ImageUploader, did image preview

--- ttrpg-grid/src/App.jsx
+++ ttrpg-grid/src/App.jsx
@@ -4,13 +4,8 @@ import ImageUploader from "./components/ImageUploader";
 import './style.css';
 
 function App() {
-  {/*const [imageURL, setImageURL] = useState(null)*/}
-
   return (
     <Layout>
-      < ImageUploader />
-      {/*<h1 style={{textAlign: "center" }}>TTRPG Grid Scaler</h1>*/}
-      {/*<ImageUploader onImageSelected={setImageURL} />*/}
     </Layout>
   );
 }
